b1702edc20ca887f109189c734933b93273c10a0
Dainius <silinis.dainius@gmail.com>
2023-06-01 15:39:15 +0300

    čia su groupby pasižaista

--- makinglist.py
+++ makinglist.py
@@ -9,22 +9,23 @@ for i in glob.glob((f'{dir_path}\*.zip')): zipfile.ZipFile(i).extractall(dir_pat
 for i in glob.glob((f'{dir_path}\observations-*.csv')): df = pandas.concat([df, pandas.read_csv(i, encoding='UTF-8')])
 
 df = df.convert_dtypes()
-print(df.dtypes)
+print(df.info())
 print('----------- df.dtypes ------------')
 # print(df.head())
 # print('------------ df.head -------------')
 
-unikalus = df.value_counts('taxon_id').to_frame()
+# unikalus = df.value_counts('taxon_id').to_frame()
+unikalus = df.groupby('taxon_id').count()
 # unikalus.reset_index()
 # print(unikalus.head())
 # print('--------- unikalus.head ----------')
-
+# df.to_csv(f'{dir_path}\\df.csv')
 # sujungta = pandas.merge(unikalus, df, on='taxon_id', how='left')
-joined = unikalus.join(df, on='taxon_id', how='left')
+# joined = unikalus.join(df, on='taxon_id', how='left')
 unikalus.to_csv(f'{dir_path}\\unikalus.csv')
-joined.to_csv(f'{dir_path}\joined.csv')
+# joined.to_csv(f'{dir_path}\joined.csv')
 # sujungta.to_csv(f'{dir_path}\sujungta.csv')
-print(joined.info())
+# print(joined.info())
 # print(sujungta.info())
 
 # , dtype={'taxon_id': 'Int64'}
