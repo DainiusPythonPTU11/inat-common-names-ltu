87ee4667d28b1a971b75e7b2b036f3751ae0d5d2
DainiusPythonPTU11 <silinis.dainius@gmail.com>
2023-06-02 15:45:37 +0300

    smulkūs PEP pataisymai

--- list.py
+++ list.py
@@ -1,12 +1,12 @@
 import os, glob, zipfile, pandas
 
-dir_path = f'{os.getcwd()}\Files'
+dir_path = f'{os.getcwd()}\\Files'
 
-for i in glob.glob((f'{dir_path}\\*.zip')): zipfile.ZipFile(i).extractall(dir_path)
+for i in glob.glob(f'{dir_path}\\*.zip'): zipfile.ZipFile(i).extractall(dir_path)
 
 df = pandas.DataFrame()
 
-for i in glob.glob((f'{dir_path}\\observations-*.csv')): df = pandas.concat([df, pandas.read_csv(i, encoding='UTF-8')])
+for i in glob.glob(f'{dir_path}\\observations-*.csv'): df = pandas.concat([df, pandas.read_csv(i, encoding='UTF-8')])
 
 df = df.convert_dtypes().reset_index(drop=True)
 
@@ -14,10 +14,10 @@ df = df.convert_dtypes().reset_index(drop=True)
 
 df = df.drop(columns=['created_at', 'species_guess'])
 
-tc = df.value_counts('taxon_id').to_frame()
+uv = df.value_counts('taxon_id').to_frame()
 
 df = df.drop_duplicates()
 
 df['common_name'] = df['common_name'].str.capitalize()
 
-tc.join(df.set_index('taxon_id'), on='taxon_id').sort_values(by='scientific_name').to_csv(f'{dir_path}\\apjungtas.csv')
+uv.join(df.set_index('taxon_id'), on='taxon_id').sort_values(by='scientific_name').to_csv(f'{dir_path}\\apjungtas.csv')
