96134ace9f2427cbcdcfc31e2c703480c47dd501
DainiusPythonPTU11 <silinis.dainius@gmail.com>
2023-06-03 17:01:57 +0300

    Veikia

--- list.py
+++ list.py
@@ -2,26 +2,31 @@ import os, glob, zipfile, pandas
 
 dir_path = f'{os.getcwd()}\\Files'
 
-for i in glob.glob(f'{dir_path}\\*.zip'): zipfile.ZipFile(i).extractall(dir_path)
+for i in glob.glob(f'{dir_path}\\*.zip'):
+    zipfile.ZipFile(i).extractall(dir_path)
+    print(i)
 
 df = pandas.DataFrame()
 
-for i in glob.glob(f'{dir_path}\\observations-*.csv'): df = pandas.concat([df, pandas.read_csv(i, encoding='UTF-8')])
+for i in glob.glob(f'{dir_path}\\observations-*.csv'):
+    df = pandas.concat([df, pandas.read_csv(i, encoding='UTF-8', low_memory=False).drop(columns=['species_guess'])])
+    print(i)
 
 df = df.convert_dtypes().reset_index(drop=True)
 
-df.sort_values(by='created_at').to_csv(f'{dir_path}\\df.csv') # Čia
-print('\n', '                             Paskutinio stebėjimo laikas:', max(df['created_at']))
-data = input('Nuo kurios dienos nereikia įtraukti duomenų? (YYYY-MM-DD): ')+' 00:00:00+0000'
-print('                            Nebus įtraukiami duomenys nuo:', data)
+print('\n', '       Paskutinio stebėjimo laikas:', pandas.to_datetime(max(df['created_at']), utc=True))
+data = input('Neįtraukti duomenų nuo (YYYY-MM-DD): ')+' 00:00:00+00:00'
+print('        Nebus įtraukti duomenys nuo:', data)
 
 df['created_at'] = pandas.to_datetime(df['created_at'], utc=True)
 
+df.sort_values(by='created_at').reset_index(drop=True).to_csv(f'{dir_path}\\df.csv')
+
 df = df[df.created_at < data]
 
-df.to_csv(f'{dir_path}\\dfaltered.csv')
+df.sort_values(by='created_at').reset_index(drop=True).to_csv(f'{dir_path}\\dfaltered.csv')
 
-df = df.drop(columns=['created_at', 'species_guess'])
+df = df.drop(columns=['created_at'])
 
 uv = df.value_counts('taxon_id').to_frame()
 
