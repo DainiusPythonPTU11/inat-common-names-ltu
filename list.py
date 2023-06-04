35313e7d5a683b80651538db78da110602ba54a6
DainiusPythonPTU11 <silinis.dainius@gmail.com>
2023-06-04 23:13:42 +0300

    apstvarkiau

--- list.py
+++ list.py
@@ -1,30 +1,39 @@
-import os, glob, zipfile, pandas
+from os import getcwd
+from glob import glob
+from zipfile import ZipFile
+import pandas as pd
 
-dir_path = f'{os.getcwd()}\\Files'
-df = pandas.DataFrame()
+dir_path = f'{getcwd()}\\Files'
 
-for i in glob.glob(f'{dir_path}\\*.zip'):
-    zipfile.ZipFile(i).extractall(dir_path)
-    latest_file = i[i.find('observations-') + 13:i.find('.csv')]
+i = ''
 
-for i in glob.glob(f'{dir_path}\\observations-*.csv'):
-    df = pandas.concat([df, pandas.read_csv(i, encoding='UTF-8', low_memory=False).drop(columns=['species_guess'])])
+for i in glob(f'{dir_path}\\*.zip'):
+    ZipFile(i).extractall(dir_path)
+
+latest_file = i[i.find('observations-') + 13:i.find('.csv')]
+
+df = pd.DataFrame()
+
+for i in glob(f'{dir_path}\\observations-*.csv'):
+    df = pd.concat([df, pd.read_csv(i, encoding='UTF-8', low_memory=False).drop(columns=['species_guess'])])
 
 df = df.convert_dtypes().reset_index(drop=True)
 
-print('\n', '       Paskutinio stebėjimo laikas:', pandas.to_datetime(max(df['created_at']), utc=True))
+print('\n', '       Paskutinio stebėjimo laikas:', pd.to_datetime(max(df['created_at']), utc=True))
 data = input('Neįtraukti duomenų nuo (YYYY-MM-DD): ') + ' 00:00:00+00:00'
 
-df['created_at'] = pandas.to_datetime(df['created_at'], utc=True)
+df['created_at'] = pd.to_datetime(df['created_at'], utc=True)
+
 df = df[df.created_at < data]
 
-latest_datetime = str(pandas.to_datetime(max(df['created_at']), utc=True))[:19].replace(':', '_') + ' UTC'
+latest_datetime = str(pd.to_datetime(max(df['created_at']), utc=True))[:19].replace(':', '_') + ' UTC'
 
 df = df.drop(columns=['created_at'])
 
 taxon_count = df.value_counts('taxon_id').to_frame()
 
 df = df.drop_duplicates()
+
 df['common_name'] = df['common_name'].str.capitalize()
 
 taxon_count.join(df.set_index('taxon_id'), on='taxon_id').sort_values(by='scientific_name').to_excel(
